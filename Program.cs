8ec82930fec1505693f5be3797c75290a9764bbd
Bohdan <bohdanrubell@gmail.com>
2023-02-15 13:39:04 +0200

    Added choise of Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace program;
 
@@ -6,27 +7,41 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        
+
         while (true)
         {
-            Console.Write("Enter the first num: ");
-            double firstNumber = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter the second num: ");
-            double secondNumber = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter the sign: ");
-            string sign = Console.ReadLine();
+            Console.WriteLine("Would you like start the program? Write (YES) to confirm or (NO) to stop");
+            string choise = Console.ReadLine();
+            if (choise.ToUpper() == "YES")
+            {
+                Console.Write("Enter the first num: ");
+                double firstNumber = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the second num: ");
+                double secondNumber = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the sign: ");
+                string sign = Console.ReadLine();
 
-            MathProxy test = new MathProxy();
-            double res = 0;
-            if (sign == "+")
-            {
-                res = test.add(firstNumber, secondNumber);
-                Console.WriteLine("{0} {1} {2} = {3}", firstNumber, sign, secondNumber, res);
+                MathProxy test = new MathProxy();
+                double res = 0;
+                if (sign == "+")
+                {
+                    res = test.add(firstNumber, secondNumber);
+                    Console.WriteLine("{0} {1} {2} = {3}", firstNumber, sign, secondNumber, res);
+                }
+                else if (sign == "-")
+                {
+                    res = test.sub(firstNumber, secondNumber);
+                    Console.WriteLine("{0} {1} {2} = {3}", firstNumber, sign, secondNumber, res);
+                }
             }
-            else if (sign == "-")
+            else if (choise.ToUpper() == "NO")
             {
-                res = test.sub(firstNumber, secondNumber);
-                Console.WriteLine("{0} {1} {2} = {3}", firstNumber, sign, secondNumber, res);
+                Console.WriteLine("Goodbye!");
+                break;
             }
+            
+           
         }
     }    
 }
